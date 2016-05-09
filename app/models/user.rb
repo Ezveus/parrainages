8c0108d3f533cf529a704b2ecf0c31dca2627452
Matthieu Ciappara <ciappam@gmail.com>
2016-05-09 20:19:55 +0200

    Added convenience methods on User to find different types of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,31 @@
 class User < ActiveRecord::Base
-  has_many :proteges, class_name: "User", foreign_key: "sponsor_id"
-  belongs_to :sponsor, class_name: "User"
+  has_many :proteges, class_name: 'User', foreign_key: 'sponsor_id'
+  belongs_to :sponsor, class_name: 'User'
+
+  def self.with_no_proteges
+    self.joins('LEFT JOIN users proteges ON proteges.sponsor_id = users.id').where('proteges.ID IS null')
+  end
+
+  # Do not use #size on this because it creates a SQL error. #length loads the query and returns expected result
+  # This is because of the #group clause
+  def self.with_more_than_x_proteges(x)
+    self.joins('LEFT JOIN users proteges ON proteges.sponsor_id = users.id').group(:id)
+        .having('COUNT(proteges.id) > ?', x)
+  end
+
+  # Do not use #size on this because it creates a SQL error. #length loads the query and returns expected result
+  # This is because of the #group clause
+  def self.who_sponsored_most_people(max_number_of_sponsors)
+    self.joins('LEFT JOIN users proteges ON proteges.sponsor_id = users.id').group(:id)
+        .order('COUNT(proteges.id) DESC').limit(max_number_of_sponsors)
+  end
+
+  def delete_with_responsoring
+    self.proteges.find_each { |protege| protege.update(sponsor: self.sponsor) }
+    self.destroy
+  end
+
+  def initial_sponsor
+    self.sponsor.nil? ? self : self.sponsor.initial_sponsor
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,15 +1,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  GDURAND = "gdurand"
-  MDUPONT = "mdupont"
+  GDURAND = 'gdurand'
+  MDUPONT = 'mdupont'
+  HBOUCHET = 'hbouchet'
+  BBERNARD = 'bbernard'
+  FPERRIN = 'fperrin'
+  MBOULANGER = 'mboulanger'
+  CDUBOIS = 'cdubois'
 
-  test "there is two users" do
-    assert User.all.size == 2
-  end
-
-  test "the last user's login is #{MDUPONT}" do
-    assert_equal MDUPONT, User.last.login
+  setup do
+    @gdurand = users(:gdurand)
+    @mdupont = users(:mdupont)
+    @hbouchet = users(:hbouchet)
+    @bbernard = users(:bbernard)
+    @fperrin = users(:fperrin)
+    @mboulanger = users(:mboulanger)
+    @cdubois = users(:cdubois)
   end
 
   test "there is a user whose login is #{GDURAND}" do
@@ -19,19 +26,56 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "#{MDUPONT} has no sponsor" do
-    user = User.find_by(login: MDUPONT)
-    refute_nil user
-
-    sponsor = user.sponsor
+    sponsor = @mdupont.sponsor
     assert_nil sponsor
   end
 
   test "#{GDURAND} is sponsored by #{MDUPONT}" do
-    user = User.find_by(login: GDURAND)
-    refute_nil user
-
-    sponsor = user.sponsor
+    sponsor = @gdurand.sponsor
     refute_nil sponsor
     assert_equal MDUPONT, sponsor.login
+  end
+
+  test "#{MDUPONT} has 2 proteges" do
+    assert_equal 2, @mdupont.proteges.size
+  end
+
+  test 'there is 5 people who sponsored noone' do
+    assert_equal 5, User.with_no_proteges.size
+  end
+
+  test 'there is 3 people who sponsored more than 1 protege' do
+    assert_equal 3, User.with_more_than_x_proteges(1).length
+  end
+
+  test "the 2 people who sponsored the most of proteges are #{HBOUCHET} and #{GDURAND}" do
+    sponsors = User.who_sponsored_most_people(2).map { |sponsor| sponsor.login }
+    assert_equal 2, sponsors.size
+    assert_includes sponsors, HBOUCHET
+    assert_includes sponsors, GDURAND
+  end
+
+  test "the initial sponsor of #{MDUPONT} is himself" do
+    assert_equal MDUPONT, @mdupont.initial_sponsor.login
+  end
+
+  test "the initial sponsor of #{MBOULANGER} is #{MDUPONT}" do
+    assert_equal MDUPONT, @mboulanger.initial_sponsor.login
+  end
+
+  test "when #{MDUPONT} is deleted, #{GDURAND} and #{BBERNARD} have no sponsor anymore" do
+    @mdupont.delete_with_responsoring
+    assert_nil @gdurand.sponsor
+    assert_nil @bbernard.sponsor
+  end
+
+  test "when #{HBOUCHET} is deleted, #{MBOULANGER}, #{CDUBOIS} and #{FPERRIN} have #{GDURAND} as sponsor" do
+    @hbouchet.delete_with_responsoring
+    @mboulanger = User.find_by(login: MBOULANGER)
+    @cdubois = User.find_by(login: CDUBOIS)
+    @fperrin = User.find_by(login: FPERRIN)
+    assert_equal GDURAND, @mboulanger.sponsor.login
+    assert_equal GDURAND, @cdubois.sponsor.login
+    assert_equal GDURAND, @fperrin.sponsor.login
   end
 end
